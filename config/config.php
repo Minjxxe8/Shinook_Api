c7848252d7d0e516db6d18b2e0841a1de3faff66
Lénaaaa <ricardlena8@gmail.com>
2026-02-25 20:04:42 +0100

    feature: Creating the server

--- config/config.php
+++ config/config.php
@@ -1,2 +1,25 @@
 <?php
-//Constantes BDD, Rôle et URL
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'darksouls_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
+
+define('BASE_URL', 'http://localhost/shinook/public/');
+define('ROOT_PATH', dirname(__DIR__));
+
+define('ROLE_USER',  'villageois');
+define('ROLE_ADMIN', 'Team Nook');
+
+// Démarrage de session
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 3600,
+        'path'     => '/',
+        'secure'   => false,
+        'httponly' => true,
+        'samesite' => 'Strict',
+    ]);
+    session_start();
+}
